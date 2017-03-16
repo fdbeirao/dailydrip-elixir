23f069b32bcb7780e08d01efa5475794e50b73ea
Josh Adams <josh.rubyist@gmail.com>
2017-03-16 12:40:51 -0500

    Don't use a macro if you can avoid it

--- rpn/lib/rpn.ex
+++ rpn/lib/rpn.ex
@@ -3,24 +3,24 @@ defmodule Rpn do
   A Reverse Polish calculator
   """
 
-  defmacro apply_operation_to_state(operation) do
-    quote do: fn(state) -> run_operation(state, unquote(operation)) end
+  defp apply_operation(pid, operation) do
+    Agent.update(pid, fn(state) -> run_operation(state, operation) end)
   end
 
-  def start(), do: 
+  def start(), do:
     Agent.start(fn -> [] end)
 
   def peek(pid), do:
     Agent.get(pid, fn(value) -> value end)
 
   def push(pid, :+), do:
-    Agent.update(pid, apply_operation_to_state(&+/2))
+    apply_operation(pid, &+/2)
 
   def push(pid, :-), do:
-    Agent.update(pid, apply_operation_to_state(&-/2))
+    apply_operation(pid, &-/2)
 
   def push(pid, :x), do:
-    Agent.update(pid, apply_operation_to_state(&*/2))
+    apply_operation(pid, &*/2)
 
   def push(pid, number) when is_integer(number), do:
     Agent.update(pid, fn(state) -> [number | state] end)
